a1d1d7955d702dafab509251afa62c4158209912
Eshkhuvvatoff <Lucifer.20080905@gmail.com>
2024-09-13 17:40:32 +0500

    add gta V

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -14,7 +14,7 @@ const Games = () => {
           <div className="card-body flex pl-10 text-center">
             <h2 className="card-title">BattleField 5</h2>
             <div className="card-actions inline-block" >
-              <a href="../assets/Apps/808_Battlefield_V_b.torrent" download>
+              <a href="\src\assets\Apps\[Eshkhuvvatoff] BattleField 5.torrent" download>
                 <button className="button" type="button">
                   <span className="button__text">Download</span>
                   <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
@@ -33,7 +33,26 @@ const Games = () => {
           <div className="card-body flex pl-10 text-center">
             <h2 className="card-title">Counter-Strike Global Offensive</h2>
             <div className="card-actions inline-block" >
-              <a href="../assets/Apps/CSGO.torrent" download>
+              <a href="\src\assets\Apps\[Eshkhuvvatoff] CSGO.torrent" download>
+                <button className="button" type="button">
+                  <span className="button__text">Download</span>
+                  <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhtbYRYv-VHQb3VZMunwVr4dm8UAGmzK_qHQ&s"
+              alt="game photo"
+              className="rounded-xl " />
+          </figure>
+          <div className="card-body flex pl-10 text-center">
+            <h2 className="card-title">Counter-Strike Global Offensive</h2>
+            <div className="card-actions inline-block" >
+              <a href="\src\assets\Apps\[Eshkhuvvatoff] Gta V.torrent" download>
                 <button className="button" type="button">
                   <span className="button__text">Download</span>
                   <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
